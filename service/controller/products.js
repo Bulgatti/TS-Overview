6cdb1ee9b66a17514f8265dd6edae5c4cf637f42
Robert Hu <robert.y.hu.97@gmail.com>
2023-01-02 14:16:30 -0800

    modified controller and model for getProducts. added linter rule

--- service/controller/products.js
+++ service/controller/products.js
@@ -1,10 +1,13 @@
 const model = require('../model');
 
 const getProducts = (req, res) => {
-
-  model.products.getProducts
-    .then((data) => res.status(200).send(data))
-    .catch((err) => res.status(500).send(err));
+  model.products(req.query.page, req.query.count, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
 };
 
 module.exports = getProducts;
--- service/model/products.js
+++ service/model/products.js
@@ -1,7 +1,12 @@
 const db = require('../db');
 
-// const getProducts =
-
-module.exports = {
-  getProducts: db.query('SELECT * FROM product WHERE id=1'),
+const getProducts = async (page = 1, count = 5, cb) => {
+  try {
+    const data = await db.query(`SELECT * FROM product WHERE id > ${(page - 1) * count} LIMIT ${count}`);
+    cb(null, data.rows);
+  } catch (err) {
+    cb(err);
+  }
 };
+
+module.exports = getProducts;
